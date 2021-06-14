048e8fbb1fd5d15dc4530b7e98c2230a5ae21342
caesaragen <caesaragen@gmail.com>
2021-06-14 22:50:15 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Footer from './components/Footer/footer';
+import Home from './pages/Home';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <Router>
      <Navbar />
+     <Sidebar />
+     <Home />
      <Footer />
     </Router>
   );
